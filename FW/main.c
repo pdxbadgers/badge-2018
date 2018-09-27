10eef0c35fd5feee6d20beb1015a8168a174e4c2
office-deskjet <office_deskjet@protonmail.com>
2018-09-26 23:40:56 -0700

    blink light

--- FW/main.c
+++ FW/main.c
@@ -1,6 +1,17 @@
+#define F_CPU 8000000UL
+
+#include <util/delay.h>
 #include <avr/io.h>
 
 int main()
 {
+    DDRA = (1 << PA1) | (1 << PA0);
+    PORTA = 0x02;
 
+    for(;;) {
+        PORTA |= (1 << PA0);
+        _delay_ms(500);
+        PORTA &= ~(1 << PA0);
+        _delay_ms(500);
+    }
 }
