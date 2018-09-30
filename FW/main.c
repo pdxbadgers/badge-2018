a941b07e925267013096980d8830e8101dc7c401
office-deskjet <office_deskjet@protonmail.com>
2018-09-30 01:17:00 -0700

    added led documentation, reading fuse bits

--- FW/main.c
+++ FW/main.c
@@ -1,17 +1,24 @@
-#define F_CPU 8000000UL
+#define F_CPU 1000000UL
 
 #include <util/delay.h>
 #include <avr/io.h>
 
+#define ROW_1 (1 << PA0)
+#define ROW_2 (1 << PA1)
+#define ROW_3 (1 << PA2)
+#define ROW_4 (1 << PA3)
+
+
 int main()
 {
-    DDRA = (1 << PA1) | (1 << PA0);
-    PORTA = 0x02;
+    DDRA = ROW_4 | ROW_3 | ROW_2 | ROW_1;
+
+    char LED[D0, D1, D2];
 
     for(;;) {
-        PORTA |= (1 << PA0);
+        PORTA |= (1 << PA2);
         _delay_ms(500);
-        PORTA &= ~(1 << PA0);
+        PORTA &= ~(1 << PA2);
         _delay_ms(500);
     }
 }
