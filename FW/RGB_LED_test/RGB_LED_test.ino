1e66ea76077d5107905cb2324d1d60e6e69755e7
office-deskjet <office_deskjet@protonmail.com>
2018-10-22 21:54:45 -0700

    code working for Red channel

--- FW/RGB_LED_test/RGB_LED_test.ino
+++ FW/RGB_LED_test/RGB_LED_test.ino
@@ -1,45 +1,59 @@
- const int EN_RGB1 = 10; // PA4
-const int EN_RGB2 = 11; // PA5
-const int EN_RGB3 = 12; // PA6
-const int EN_RGB4 = 13; // PA7
+#include <util/delay.h>
+
+const int EN_RGB1 = (1 << PA4); //10; // PA4
+const int EN_RGB2 = (1 << PA5); //11; // PA5
+const int EN_RGB3 = (1 << PA6); //12; // PA6
+const int EN_RGB4 = (1 << PA7); //13; // PA7
 const int R_PWM = 8; // PB1
 const int G_PWM = 6; // PB3
 const int B_PWM = 4; // PB5
 
-byte red_pwm[10] = {26, 52, 78, 104, 130, 156, 182, 208, 234, 255};
+const byte NUM_RGB_LEDs = 4;
+const byte PWM_MAX = 255;
 
-byte EN_RGB[4] = {EN_RGB1, EN_RGB2, EN_RGB3, EN_REGB4};
+byte en_rgb_led[NUM_RGB_LEDs] = {EN_RGB1, EN_RGB2, EN_RGB3, EN_RGB4};
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(EN_RGB1, OUTPUT);
-  pinMode(EN_RGB2, OUTPUT);
-  pinMode(EN_RGB3, OUTPUT);
-  pinMode(EN_RGB4, OUTPUT);
-  pinMode(R_PWM, OUTPUT);
-  pinMode(G_PWM, OUTPUT);
-  pinMode(B_PWM, OUTPUT);
+void setup()
+{
+    DDRA = EN_RGB4 | EN_RGB3 | EN_RGB2 | EN_RGB1;
 
-  // Cleared on compare match, enable PWM on OC1A
-  TCCR1A = (1 << COM1A1) | (1 << PWM1A); 
-  
-  // Set presacle to CLK/ -> PWM Freq 3.926Khhz
-  TCCR1B = (1 << CS11) | (1 << CS10);
-  //OCR1A = 66;
-  
-  digitalWrite(EN_RGB1, HIGH);
-  digitalWrite(G_PWM, HIGH);
-  digitalWrite(B_PWM, HIGH);
-  //digitalWrite(EN_RGB2, HIGH);
-  
+    /*
+    pinMode(EN_RGB1, OUTPUT);
+    pinMode(EN_RGB2, OUTPUT);
+    pinMode(EN_RGB3, OUTPUT);
+    pinMode(EN_RGB4, OUTPUT);
+*/
+    pinMode(R_PWM, OUTPUT);
+    pinMode(G_PWM, OUTPUT);
+    pinMode(B_PWM, OUTPUT);
+
+    // Cleared on compare match, enable PWM on OC1A
+    TCCR1A = (1 << COM1A1) | (1 << PWM1A); 
+
+    // Set presacle to CLK/ -> PWM Freq 3.926Khhz
+    TCCR1B = (1 << CS11) | (1 << CS10);
+
+    // turn off LED to start
+    //digitalWrite(R_PWM, HIGH);
+    digitalWrite(G_PWM, HIGH);
+    digitalWrite(B_PWM, HIGH);
 }
 
-void loop() {
-  int i;
+void loop()
+{
+    byte led;
+    byte color
+    byte i;
 
-  for (i = 0; i < 255; i+=4) {
-    OCR1A = 255 - i;
-    delay(100);
-  }
-  OCR1A = 0;
+    // loop through all RGB LEDs pulsing red
+    for (led = 0; led < NUM_RGB_LEDs; led++)
+    {
+        PORTA |= en_rgb_led[led];
+        for (i = 0; i < PWM_MAX; i++)
+        {
+            OCR1A = PWM_MAX - i;
+            _delay_ms(4);
+        }
+        PORTA &= ~(en_rgb_led[led]);
+    }
 }
