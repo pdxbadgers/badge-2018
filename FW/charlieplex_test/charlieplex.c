44302afeac70a2a957da0a16a4d91e2d633c2ff1
office-deskjet <office_deskjet@protonmail.com>
2018-10-23 18:31:57 -0700

    converting arduino code to avr-libc

--- FW/charlieplex_test/charlieplex.c
+++ FW/charlieplex_test/charlieplex.c
@@ -1,22 +1,14 @@
 // Test the charlieplexing and LED mapping
 #define F_CPU 8000000UL
-//#include <avr/io.h>
-//#include <util/delay.h>
-//#include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
 
 // pin definitions
-
-#define ROW_1 (1 << PA0) // 0
-#define ROW_2 (1 << PA1) // 1
-#define ROW_3 (1 << PA2) // 2
-#define ROW_4 (1 << PA3) // 14
-
-
-
-#define ROW1  0
-#define ROW2  1
-#define ROW3  2
-#define ROW4  14
+#define ROW1 (1 << PA0) // 0
+#define ROW2 (1 << PA1) // 1
+#define ROW3 (1 << PA2) // 2
+#define ROW4 (1 << PA3) // 14
 
 #define ANODE 0
 #define CATHODE 1
@@ -26,58 +18,45 @@
 #define MED_SPEED 250
 #define FAST_SPEED 75
 
-volatile int SPEED = 1000;
+volatile uint16_t SPEED = 1000;
 
 // array of all the LEDs in {ANODE,CATHODE} pairing
-char LED[NUMB_LEDS][2] = { {ROW3, ROW2}, {ROW2, ROW3}, {ROW4, ROW3},
+uint8_t LED[NUMB_LEDS][2] = { {ROW3, ROW2}, {ROW2, ROW3}, {ROW4, ROW3},
                            {ROW3, ROW4}, {ROW4, ROW2}, {ROW2, ROW4},
                            {ROW4, ROW1}, {ROW1, ROW4}, {ROW3, ROW1},
                            {ROW1, ROW3}, {ROW2, ROW1}, {ROW1, ROW2},
 };
 
-void setup()
-{
-    all_off_led();
-}
-
-void loop()
-{
-    cycle_led();
-}
-
 // turn off all LEDs
 void all_off_led()
 {
-    // high Z for all pins
-//    DDRA &= ~ROW4 & ~ROW3 & ~ROW2 & ~ROW1;
- //   PORTA &= ~ROW4 & ~ROW3 & ~ROW2 & ~ROW1;
-    pinMode(ROW1, INPUT);
-    pinMode(ROW2, INPUT);
-    pinMode(ROW3, INPUT);
-    pinMode(ROW4, INPUT);
+    // high Z for all ROW pins
+    DDRA &= ~ROW4 & ~ROW3 & ~ROW2 & ~ROW1;
+    PORTA &= ~ROW4 & ~ROW3 & ~ROW2 & ~ROW1;
 }
 
+
 // turn on a given LED, param is a an {ANODE,CATHODE} pair
-void on_led(char *LED)
+void on_led(uint8_t *LED)
 {
-    pinMode(LED[ANODE], OUTPUT);
-    pinMode(LED[CATHODE], OUTPUT);
+    // Set pins to output
+    DDRA |= LED[ANODE] | LED[CATHODE];
 
     digitalWrite(LED[ANODE], HIGH);
     digitalWrite(LED[CATHODE], LOW);
 }
 
 // turn off a given LED, param is a an {ANODE,CATHODE} pair
-void off_led(char *LED)
+void off_led(uint8_t *LED)
 {
     pinMode(LED[ANODE], INPUT);
     pinMode(LED[CATHODE], INPUT);
 }
 
-void blink_led(char *LED)
+void blink_led(uint8_t *LED)
 {
     on_led(LED);
-    delay(SPEED);
+    _delay_ms(SPEED);
 
     off_led(LED); 
 }
@@ -88,4 +67,10 @@ void cycle_led()
 
     for (i = 0; i < NUMB_LEDS; i++)
         blink_led(LED[i]);
+}
+
+int main()
+{
+    all_off_led();
+    cycle_led();
 }
